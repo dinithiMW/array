781ab1dfc561624bb4e190c4bf1b1212bb955e10
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-02-19 22:06:12 +0530

    array
    array

---
+++ Array.java
@@ -0,0 +1,14 @@
+
+public class Array {
+    public static void main(String args[]){
+    
+        int[]arg={20,30,40,12,45,60,20,50,56,34};
+        
+    
+    
+    for(int i=0;i< arg.length;i++){
+     System.out.println(arg[i]);
+}
+    
+}
+}
